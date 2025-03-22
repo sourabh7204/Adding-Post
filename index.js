03221995b06814e113ce80b6b0456db52e076c4d
Sourabh Vishwakarma <sourabh722004@gmail.com>
2025-03-22 22:17:05 +0530

    Added Serch Function

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const methodOverride = require("method-override");
-
-
 
 app.use(express.urlencoded({
     extended: true
 }));
-
 app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
@@ -19,71 +16,93 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
-        id:uuidv4(),
+        id: uuidv4(),
         username: "Sourabh",
         content: "Consistency is the Key!",
+        likes: 0,
     },
     {
-        id:uuidv4(),
+        id: uuidv4(),
         username: "Sankeerth",
         content: "Hard Work is important to achieve Success...",
+        likes: 0,
     },
     {
-        id:uuidv4(),
+        id: uuidv4(),
         username: "Rohith",
         content: "I got selected for my 1st internship!",
+        likes: 0,
     },
 ];
 
+// Display all posts
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
 });
 
-
-
+// Create a new post
 app.get("/posts/new", (req, res) => {
     res.render("new.ejs");
 });
-
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
     let id = uuidv4();
-    posts.push({ id, username, content });
+    posts.push({ id, username, content, likes: 0 });
     res.redirect("/posts");
 });
 
+// Search posts
+app.get("/posts/search", (req, res) => {
+    let { query } = req.query;
+    let filteredPosts = posts.filter((p) =>
+        p.content.toLowerCase().includes(query.toLowerCase())
+    );
+    res.render("search.ejs", { posts: filteredPosts });
+});
 
 
+// Like a post
+app.patch("/posts/:id/like", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => p.id === id);
+    if (post) {
+        post.likes++;
+    }
+    res.redirect("/posts");
+});
+
+// View a single post
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
-    console.log(id);
-    let post = posts.find((p) => id === p.id);
+    let post = posts.find((p) => p.id === id);
     res.render("show.ejs", { post });
-  });
-  
-app.patch("/posts/:id", (req, res) =>{
-    let{id}=req.params;
+});
+
+// Update post content
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
     let newContent = req.body.content;
-    let post = posts.find((p) => id === p.id);
-    post.content = newContent;
-    console.log(post);
-    res.redirect("/posts/");
+    let post = posts.find((p) => p.id === id);
+    if (post) {
+        post.content = newContent;
+    }
+    res.redirect("/posts");
 });
 
-app.get("/posts/:id/edit",(req,res)=>{
+// Edit post
+app.get("/posts/:id/edit", (req, res) => {
     let { id } = req.params;
-    let post = posts.find((p) => id === p.id);
-    res.render("edit.ejs", {post});
+    let post = posts.find((p) => p.id === id);
+    res.render("edit.ejs", { post });
 });
 
-app.delete("/posts/:id", (req,res)=>{
+// Delete post
+app.delete("/posts/:id", (req, res) => {
     let { id } = req.params;
-    posts = posts.filter((p) => id !== p.id);
-    res.redirect("/posts/");
+    posts = posts.filter((p) => p.id !== id);
+    res.redirect("/posts");
 });
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 });
-
-
